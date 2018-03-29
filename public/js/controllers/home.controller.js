bba65a728bf15eeefd46d25b84d61563174d7bda
matheus-lima92 <lima.eca@gmail.com>
2018-03-29 19:31:56 -0300

    console.logs removed from controller

--- public/js/controllers/home.controller.js
+++ public/js/controllers/home.controller.js
@@ -1,11 +1,8 @@
 const API_URL='http://localhost:3000'
 
 myApp.controller('homeController', ($scope, $http, toastr) => {
-	console.log("Angular working!")
 
 	$scope.isPalindrome = () => {
-		console.log('teste')
-		console.log($scope.userInput);
 		$http.get(`${API_URL}/palindrome?sentence=${$scope.userInput}`)
 			.then(response => {
 				toastr.success($scope.userInput, response.data)
